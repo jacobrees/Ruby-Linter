976c570ddab02db465a03dfa871ad7d19c2f22b2
jacobrees <jacob.rees97@yahoo.com>
2021-10-15 23:39:30 +0100

    Fix bug that stopped program from running

--- bin/main.rb
+++ bin/main.rb
@@ -6,7 +6,7 @@ require_relative '../lib/read_all'
 require_relative '../lib/space_test'
 require_relative '../lib/launch_linter'
 
-puts `Launching DragonLintAlpha...`.red
+puts 'Launching DragonLintAlpha...'.red
 puts 'Please enter which Ruby file from bin/error/ directory you would like to scan.'
 puts 'Enter "*" to scan all .rb files inside bin/error directory.'
 
